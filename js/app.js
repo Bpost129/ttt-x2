d57a2c145bff02af8f5b821262c17cc2b21368c9
Bpost129 <Bpost129@yahoo.com>
2025-11-01 19:53:35 -0400

    add variables for choices, messsage, and winning combos, add event listeners to board elements, create function to save and display player choices

---
+++ js/app.js
@@ -0,0 +1,45 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let msg = 'Wanna play?'
+let playerChoices = [], cpuChoices = []
+
+/*------------------------ Cached Element References ------------------------*/
+
+const boardEl = document.querySelector('.board')
+const messageEl = document.querySelector('#message')
+
+messageEl.textContent = msg
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.querySelectorAll("div").forEach(function (square) {
+  square.addEventListener("click", playerSelect)
+})
+
+/*-------------------------------- Functions --------------------------------*/
+
+function playerSelect(e) {
+  if (!playerChoices.includes(e.target.id[2])) {
+    playerChoices.push(e.target.id[2])
+    e.target.textContent = 'X'
+    e.target.removeEventListener('click', playerSelect)
+  }
+  console.log(playerChoices)
+}
+
+function cpuSelect() {
+  
+}
